4db5d9ff25b32a49acecf78e5a0e5092703e8c5e
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-09-01 23:43:04 -0600

    [INITIALIZING] - Project with Spring boot, web, data & security

---
+++ src/main/java/com/projectRs/sgrs/SgrsApplication.java
@@ -0,0 +1,13 @@
+package com.projectRs.sgrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectRs/sgrs/SgrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectRs.sgrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
