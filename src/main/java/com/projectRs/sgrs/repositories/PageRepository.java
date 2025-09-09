27d4b49b3cc644c4ec33c25a799964ab43c32a42
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-09-09 00:39:15 -0600

    [ADD] - Query Method in PageRepository.java

--- src/main/java/com/projectRs/sgrs/repositories/PageRepository.java
+++ src/main/java/com/projectRs/sgrs/repositories/PageRepository.java
@@ -3,6 +3,11 @@ package com.projectRs.sgrs.repositories;
 import com.projectRs.sgrs.entities.PageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PageRepository extends JpaRepository<PageEntity, Long> {
 
+    // SELECT * FROM page WHERE title = :title
+    // @Query("from PageEntity where title = :title") //JPQL
+    Optional<PageEntity> findByTitle(String title);
 }
